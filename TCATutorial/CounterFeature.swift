c37140939103189c47cfde8e4c0f318f42fe3538
Keisuke2162 <applabo.kei@gmail.com>
2023-10-11 23:43:36 +0900

    CounterFeature実装

--- TCATutorial/CounterFeature.swift
+++ TCATutorial/CounterFeature.swift
@@ -5,4 +5,26 @@
 //  Created by Kei on 2023/10/11.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct CounterFeature: Reducer {
+    struct State {
+        var counter = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .decrementButtonTapped:
+            state.counter -= 0
+            return .none
+        case .incrementButtonTapped:
+            state.counter +=  0
+            return .none
+        }
+    }
+}
