782306e8274f8f1e4242c8e5e2548b5c423da74e
Keisuke2162 <applabo.kei@gmail.com>
2023-10-25 16:09:29 +0900

    Tutorial

--- TCATutorial/CounterFeature.swift
+++ TCATutorial/CounterFeature.swift
@@ -6,25 +6,41 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 struct CounterFeature: Reducer {
     struct State {
-        var counter = 0
+        var counter: Int = 0
+        var fact: String?
+        var isLoading: Bool = false
     }
 
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
     }
 
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .decrementButtonTapped:
             state.counter -= 1
+            state.fact = nil
             return .none
+
         case .incrementButtonTapped:
             state.counter +=  1
+            state.fact = nil
             return .none
+
+        case .factButtonTapped:
+            state.fact = nil
+            state.isLoading = true
+            return .run { [count = state.counter] send in
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                let fact = String(decoding: data, as: UTF8.self)
+            }
         }
     }
 }
